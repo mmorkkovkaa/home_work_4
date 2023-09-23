860a41733316a1e37f5612efa1ef90eab9d884da
Asus <lllllooooovveee@gmail.com>
2023-09-23 11:05:56 +0600

    dz4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Form(){
+    const [formData, setFormData] = useState({
+        name: "",
+        telephone: "",
+        email: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    };
+
+  return(
+      <>
+          <form>
+              <label>
+                  your name :
+                  <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}/>
+              </label>
+              <label>
+                  your phone number :
+                  <input
+                      type="tel"
+                      name="telephone"
+                      value={formData.telephone}
+                      onChange={handleChange}
+                      pattern="[0-9]*"
+                  />
+              </label>
+              <label>
+                  your email :
+                  <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}/>
+              </label>
+          </form>
+
+          <div className="preview">
+              <h3>Preview:</h3>
+              <p>Full Name: {formData.name}</p>
+              <p>Phone Number: {formData.telephone}</p>
+              <p>Email Address: {formData.email}</p>
+          </div>
+
+      </>
+  )
 }
-
-export default App;
